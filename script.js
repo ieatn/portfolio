806d127e2bf29a73b1f166329cc60fa7dab35e6f
ieatn <81874063+ieatn@users.noreply.github.com>
2023-02-22 17:28:03 -0800

    QOL changes and smooth scrolling

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ btn.addEventListener('click', () => {
     menu.classList.toggle('flex')
     menu.classList.toggle('hidden')
 })
+
+
+// scroll up btn
+const scrollTopBtn = document.getElementById('scroll-top-btn');
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
