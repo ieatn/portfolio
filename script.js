3397d77123e025dbe11b961fed874c2e670afc51
ieatn <ieatnubcake@gmail.com>
2023-03-01 11:24:18 -0800

    animated text, finishing touches projects

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+// navbar
 const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
-
 btn.addEventListener('click', () => {
-    menu.classList.toggle('flex')
-    menu.classList.toggle('hidden')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
 })
+
+// animated text
+const animatedText = document.getElementById('animatedText')
+const labels = ['Front End Developer', 'IT Student', 'Computer Nerd', 'Your Next Hire']
+
+let currentLabelIndex = -1
+
+function updateLabel() {
+  // Update the label index to the next label in the array
+  currentLabelIndex = (currentLabelIndex + 1) % labels.length
+  
+  // Get the new label text
+  const newLabelText = labels[currentLabelIndex]
+  
+  // Set the animated text content to the new label text
+  animatedText.textContent = newLabelText
+  
+  // Call updateLabel again after 5 seconds
+  setTimeout(updateLabel, 3000)
+}
+
+updateLabel() // Start the animation
+
+
+
 
 
 // scroll up btn
