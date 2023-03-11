efb9f6f60a8783ebbbea407c95aba56058340025
ieatn <ieatnubcake@gmail.com>
2023-03-10 22:13:24 -0800

    added new projects

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// navbar
+// mobile navbar
 const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
 btn.addEventListener('click', () => {
@@ -6,9 +6,41 @@ btn.addEventListener('click', () => {
   menu.classList.toggle('hidden')
 })
 
+// fixed navbar and sidebar in progress and still need to work on mobile nav
+const fixedNavbar = document.getElementById('fixedNavbar');
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 800) {
+    fixedNavbar.classList.remove('hidden');
+    fixedNavbar.classList.add('flex');
+    fixedNavbar.classList.add('translate-x-0');
+  } else {
+    fixedNavbar.classList.add('hidden');
+    fixedNavbar.classList.remove('translate-x-0');
+  }
+});
+
+// Add an event listener to your hamburger icon
+document.getElementById("fixedNavbar").addEventListener("click", function() {
+  // Toggle the 'visible' class on your sidebar element
+  document.getElementById("sidebar").classList.toggle("hidden");
+  
+  // Toggle the 'translate-x-[-10]' class on your hamburger icon to move it to the left
+  document.getElementById("fixedNavbar").classList.toggle("translate-x-[-10]");
+  
+  // Replace the image with a white 'x' when the sidebar is visible
+  if (document.getElementById("sidebar").classList.contains("hidden")) {
+    document.getElementById("fixedNavbar").src = "images/icon-hamburger.svg";
+  } else {
+    document.getElementById("fixedNavbar").src = "images/icon-close.svg";
+  }
+  
+});
+
+
 // animated text
 const animatedText = document.getElementById('animatedText')
-const labels = ['Front End Developer', 'IT Student', 'Computer Nerd', 'Your Next Hire']
+const labels = ['SOFTWARE ENGINEER', 'FRONT END DEVELOPER', 'IT STUDENT', 'COMPUTER NERD', 'YOUR NEXT HIRE']
 
 let currentLabelIndex = -1
 
@@ -22,14 +54,11 @@ function updateLabel() {
   // Set the animated text content to the new label text
   animatedText.textContent = newLabelText
   
-  // Call updateLabel again after 5 seconds
+  // Call updateLabel again after 3 seconds
   setTimeout(updateLabel, 3000)
 }
-
-updateLabel() // Start the animation
-
-
-
+// Start the animation
+// updateLabel() 
 
 
 // scroll up btn
@@ -41,3 +70,8 @@ scrollTopBtn.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+
+
+
+
